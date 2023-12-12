9a71fb46427caf38ed2e0e3c11c31d21b77ba126
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2023-12-11 18:20:39 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+  apiKey: "AIzaSyADOLtrBGlCeOjUeYwJyGqbjql3mCXVw2Q",
+    authDomain: "autprue.firebaseapp.com",
+    projectId: "autprue",
+    storageBucket: "autprue.appspot.com",
+    messagingSenderId: "558101072298",
+    appId: "1:558101072298:web:f2a4179b80a98756c5c3d3",
+    measurementId: "G-BE0EDEJ7ZS"
     });
